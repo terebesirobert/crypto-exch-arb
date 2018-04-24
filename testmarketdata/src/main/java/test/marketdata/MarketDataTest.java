b01ec4aa7569642da4890061583797d47f739a8b
unknown <terebesi.robert@yahoo.com>
2018-04-24 23:38:48 +0300

    Need some help here

---
+++ testmarketdata/src/main/java/test/marketdata/MarketDataTest.java
@@ -0,0 +1,11 @@
+package test.marketdata;
+
+public class MarketDataTest {
+
+    public static void main(String[] args) {
+
+        BitfinexApiBroker bitfinexApiBroker = new BitfinexApiBroker();
+        bitfinexApiBroker.connect();
+
+    }
+}
